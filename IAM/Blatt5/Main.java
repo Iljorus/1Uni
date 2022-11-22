3b52eb3c3ba6eb0ab3e9cd9b73afc1764f6f6312
Emil <emilkrieger@mailbox.org>
2022-11-22 17:53:50 +0100

    deleted Vector

--- IAM/Blatt5/Main.java
+++ IAM/Blatt5/Main.java
@@ -6,13 +6,7 @@ public class Main implements test, tust{
         tust second = (n, m) -> {return n+m;};
         System.out.println(first.run("Hi"));
         System.out.println(second.run(2, 3));
-
-        Vector<String> strVec=new Vector<String>();
-        strVec.set("strVec");
-        strVec.print();
-        Vector<Integer> intVec=new Vector<Integer>();
-        intVec.set(12);
-        intVec.print();
+        
     }
     public String run(String s) {
         return null;
@@ -20,4 +14,5 @@ public class Main implements test, tust{
     public int run(int i, int j){
         return 0;
     }
+
 }
