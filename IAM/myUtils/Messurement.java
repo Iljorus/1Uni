28005382e0b0c511fb082e8a7c01816aaa135227
Emil <emilkrieger@mailbox.org>
2022-11-24 21:15:43 +0100

    Added comments to Messurment.java and removed 1 method

--- IAM/myUtils/Messurement.java
+++ IAM/myUtils/Messurement.java
@@ -7,22 +7,29 @@ public class Messurement{
     public Messurement(){
         this.log=new Logger();
     }
+    /** 
+     * Starts the messurment
+    */
     public void start(){
         this.startTime=System.currentTimeMillis();
         log.toConsole("Messurment started");
     }
+    /**
+     * Ends the messurment
+     */
     public void end(){
         this.endTime=System.currentTimeMillis();
         log.toConsole("Messurment end");
     }
+    /** 
+     * Calculates the time between {@code start} and {@code end}
+     * @return String
+    */
     public String result(){
         long difference=this.endTime-this.startTime;
         if(difference<1E3)return difference+" ms";
         else if(difference>=1E3 && difference<60E6)return difference*1E3+" sec";
         else if(difference>=60E6 && difference<360E6)return difference*1E6+" min";
         else return "Fix your code";
-    }
-    public void print(String x){
-       log.toConsole("Process finished in "+x);
     }
 }
