ab5c085c8cbec640271c29def928d716cc9f25a5
Emil <emilkrieger@mailbox.org>
2022-12-01 11:55:38 +0100

    optimizing

--- IAM/Blatt6/Main.java
+++ IAM/Blatt6/Main.java
@@ -5,12 +5,8 @@ import myUtils.*;
 public class Main {
     public static void main(String[] args){
         Numbers num=new Numbers();
-    cInt a=new cInt(1, 1);
-    cInt b=new cInt(1, 2);
-    System.out.println(a.add(b).val);
-    System.out.println(a.mult(b).val);
-    System.out.println(a.div(b).val);
-    System.out.println(a.sub(b).val);
-    System.out.println(num.convert("A2E", 16, 10));
+        System.out.println(num.convert("A2E", 16, 2));
+        System.out.println(num.convert("101000101110", 2, 10));
+        System.out.println(num.convert("14", 10, 16));
     }
 }
--- IAM/myUtils/Numbers.java
+++ IAM/myUtils/Numbers.java
@@ -6,26 +6,22 @@ public class Numbers {      //Mby rename
     private String toDecimal(String x, int s){
         int result=0;
         String[] numArray=x.split("");
-        for(int i=numArray.length-1;i>=0;i--)result+=Math.pow(s, i)*conChars(String.valueOf(numArray[(numArray.length-1)-i]));
+        for(int i=numArray.length-1;i>=0;i--)result+=Math.pow(s, i)*hexValue(String.valueOf(numArray[(numArray.length-1)-i]));
         return String.valueOf(result);
     }
-    private int conChars(String x){
-        if(x.equalsIgnoreCase("A"))return 10;
-        else if(x.equalsIgnoreCase("B"))return 11;
-        else if(x.equalsIgnoreCase("C"))return 12;
-        else if(x.equalsIgnoreCase("D"))return 13;
-        else if(x.equalsIgnoreCase("E"))return 14;
-        else if(x.equalsIgnoreCase("F"))return 15;
-        else return Integer.valueOf(x);
+    private boolean isHexChar(String s){
+        return s.matches("[A-F]");
     }
-    private String conNum(String x){
-        if(x.equals("10"))return "A";
-        else if(x.equals("11"))return "B";
-        else if(x.equals("12"))return "C";
-        else if(x.equals("13"))return "D";
-        else if(x.equals("14"))return "E";
-        else if(x.equals("15"))return "F";
-        else return String.valueOf(x);
+    private boolean isNumeral(String s){
+        return s.matches("[0-9]");
+    }
+    private int hexValue(String x){
+        return "0123456789ABCDEF".indexOf(x);
+    }
+    private String toHex(String x){
+        String values="0123456789ABCDEF";   //GHIJKLMNOPQRSTUVWXYZ
+        if(Integer.valueOf(x)>=values.length())return "error";
+        return String.valueOf(values.charAt(Integer.valueOf(x)));
     }
     /**
      * Converts a number base {@code x} into base {@code y}
@@ -35,23 +31,34 @@ public class Numbers {      //Mby rename
      * @return {@code String} Converted number
      */
     public String convert(String s, int x, int y){
-        if(y==16){
-            String result="";
-            int num=Integer.valueOf(toDecimal(s, x));
-            for(int i=String.valueOf(num).length()-1;i>=0;i--){
-                result+=conNum(String.valueOf(num%16));
-                num/=16;
+        if(!isValid(s, x) || x==0 || y==0)return "error invalid input";
+        String result="";
+        int num=Integer.valueOf(toDecimal(s, x));
+        while(num>0){
+            result+=toHex(String.valueOf(num%y));
+            num/=y;
+        }
+        String[] copy=result.split("");
+        String[] cache=new String[copy.length];
+        boolean deleteZero=true;
+        for(int i=copy.length-1;i>=0;i--){
+            if(deleteZero & copy[i].equals("0"))cache[(copy.length-1)-i]="";
+            else {
+                deleteZero=false;
+                cache[(copy.length-1)-i]=copy[i];
             }
-            String[] copy=result.split("");
-            String[] cache=new String[copy.length];
-            for(int i=copy.length-1;i>=0;i--)cache[(copy.length-1)-i]=(cache[i].equals("0"))?"":copy[i];
-            return String.join("",cache);
         }
-        else{
-            int result=0;
-            String[] numArray=toDecimal(s, x).split("");
-            for(int i=numArray.length-1;i>=0;i--)result+=(int)Math.pow(y, i)*Integer.valueOf(numArray[(numArray.length-1)-i]);
-            return String.valueOf(result);
+        return String.join("",cache);
+    }
+    private boolean isValid(String s, int x){
+        if(x<1 || s.equals("null"))return false;
+        String[] numArray=s.split("");
+        if(x==16){
+            for(String str:numArray)if(!isNumeral(str) && !isHexChar(str))return false;
         }
+        else {
+            for(String str:numArray)if(isHexChar(str) || !str.matches("[0-"+String.valueOf(x-1)+"]"))return false;
+        }
+        return true;
     }
 }
